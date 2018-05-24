b2e6d356ab577aab1589adb3da7f87d78131d778
tgregit <tgregory747@gmail.com>
2018-05-24 06:20:25 -0400

    this is the best version yet, with crisper edges on the predicted images.  It is good enough to make a short animation of a cube spinning. Debating going from 10k to 20k samples in the dataset.

--- cube_data.py
+++ cube_data.py
@@ -3,13 +3,13 @@ import numpy as np
 import os
 
 
-def get_all_data(my_path, my_image_width):
+def get_all_cube_data(my_path, my_image_width):
     filenames = [os.path.join(my_path, f) for f in os.listdir(my_path) if os.path.isfile(os.path.join(my_path, f))]
     filenames.sort()
 
     total_samples = len(filenames)
 
-    x_input = np.zeros((total_samples, 4), dtype=np.float)
+    x_input = np.zeros((total_samples, 5), dtype=np.float)
     # we have 4 inputs: x_loc, z_loc, local z_rotation, and light strength -- these are already normalized 0. - 1.0
 
     y_images = np.zeros((total_samples, my_image_width, my_image_width), dtype=np.float)
@@ -20,9 +20,9 @@ def get_all_data(my_path, my_image_width):
 
         f = f[0:-4]  # delete .jpg
         all = f.split('_')
-        just_floats = np.array(all[2:6])
+        just_floats = np.array(all[2:7])
         x_input[index] = just_floats
-        #print(just_floats)
+        print(just_floats)
 
     return x_input, y_images
 
--- cube_generator.py
+++ cube_generator.py
@@ -16,25 +16,30 @@ from keras.models import load_model
 import h5py
 
 def make_generator(my_image_width):
-    loc_rot_and_light = Input(shape=(4,))
+    loc_rot_and_light = Input(shape=(5,))
 
-    hidden1 =  Dense(15, activation='relu')(loc_rot_and_light)
-    hidden2 = Dense(100, activation='relu')(hidden1)
+    hidden1 =  Dense(75, activation='tanh')(loc_rot_and_light)
+    hidden2 = Dense(150, activation='tanh')(hidden1)
     bn = BatchNormalization()(hidden2)
-    square = Dense(my_image_width * my_image_width * 1, activation='relu')(bn)
+
+    hidden3 = Dense(330, activation='relu')(bn)
+
+    #drop = Dropout(.1)(bn)
+
+    square = Dense(my_image_width * my_image_width * 1, activation='relu')(hidden3)
     reshaped = Reshape((my_image_width, my_image_width))(square)
     #conv1 = Conv2D(16, (3, 3), padding='same', activation='relu')(reshaped)
     #conv2 = Conv2D(1, (2, 2), padding='same', activation='sigmoid')(conv1)
-    reshaped2 = Reshape((my_image_width, my_image_width))(square)
+    #reshaped2 = Reshape((my_image_width, my_image_width))(square)
 
     model = Model(inputs=loc_rot_and_light, outputs=reshaped)
     print(model.summary())
     return model
 
 def write_prediction(my_gen, my_super_epoch, my_output_directory):
-    sampled_x = np.random.uniform(0, 1.0, 4)
-    x_info_str = '_' + str(sampled_x[0]) + '_' + str(sampled_x[1]) + '_' + str(sampled_x[2]) + '_' + str(sampled_x[3])
-    sampled_x = np.reshape(sampled_x, (1,4))
+    sampled_x = np.random.uniform(0, 1.0, 5)
+    x_info_str = '_' + str(sampled_x[0]) + '_' + str(sampled_x[1]) + '_' + str(sampled_x[2]) + '_' + str(sampled_x[3]) + '_' + str(sampled_x[4])
+    sampled_x = np.reshape(sampled_x, (1, 5))
     pred = my_gen.predict(sampled_x)
     im = pred[0]
     im = im * 128.0
@@ -45,16 +50,17 @@ def write_prediction(my_gen, my_super_epoch, my_output_directory):
 output_directory = '/home/foo/data/blend-new/dataset_cube/output/'
 
 image_width = 32
-x, y = cube_data.get_all_data('/home/foo/data/blend-new/dataset_cube/images', image_width)
+x, y = cube_data.get_all_cube_data('/home/foo/data/blend-new/dataset_cube/images', image_width)
 print('Total Samples: ', str(x.shape[0]))
 gen = make_generator(image_width)
 
 #opt = SGD()
-opt = Adam()#Adadelta()
+#opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)  #  from https://keras.io/optimizers/
+opt = Adam()
 gen.compile(loss='mean_squared_error', optimizer=opt)
 
 for super_epoch in range(0,5000):
-    gen.fit(x, y, epochs=25, batch_size=128, verbose=1, validation_split=.25)
+    gen.fit(x, y, epochs=100, batch_size=128, verbose=1, validation_split=.25)
 
     write_prediction(gen, super_epoch, output_directory)
 
